b57ca28a3cc73073bfb95b1159059bd9b75ef3f6
Joe McConnell <joe.mcconnell10@gmail.com>
2023-02-11 21:00:25 -0600

    first commit

---
+++ example.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+class Car {
+  public:
+    string color(string carColor);
+    string adjective();
+};
+
+string Car::adjective() {
+  string word= "sad";
+  return word;
+};
+
+string Car::color(string carColor) {
+  Car myObj;
+  carColor = "yellow";
+  string output = myObj.adjective() + " " + carColor;
+  return output;
+};
+
+int main() {
+  Car myObj; // Create an object of Car
+  cout << myObj.color("") + "\n";
+  return 0;
+}
